8c22583cfc29ebbf340fde0da10177ad5335217a
Michael Temple <mtemple@techsolutions.cc>
2014-01-17 15:08:50 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clockmeinnow::Application.config.secret_key_base = 'f2f5a56b55fcd608bf1c6a608ef8c9941c3906fed1caf3a98c837e8e6e2d6fffc850d6ed9d7a9b07ebd74c99796d77be7f5396bbbd09a896704a920753dfb5f1'
